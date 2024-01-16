7057791421444198882815a5f678545a4e17c987
Siafeson <siafeson@Mac-mini-de-Siafeson.local>
2024-01-16 13:59:04 -0700

    ...

--- app/src/main/java/com/esteban/appcomprayventa/options_login/LoginEmailActivity.kt
+++ app/src/main/java/com/esteban/appcomprayventa/options_login/LoginEmailActivity.kt
@@ -1,8 +1,11 @@
 package com.esteban.appcomprayventa.options_login
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.esteban.appcomprayventa.LoginOptions
 import com.esteban.appcomprayventa.MainActivity
 import com.esteban.appcomprayventa.R
@@ -15,14 +18,62 @@ class LoginEmailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginEmailBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginEmailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please Wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.btnSignIn.setOnClickListener {
+            validateInfo()
+        }
+
         binding.txtSigUp.setOnClickListener {
             startActivity(Intent(this@LoginEmailActivity, RegisterActivity::class.java))
         }
+    }
+
+    private var email = ""
+    private var password = ""
+    private fun validateInfo() {
+        email = binding.editTextEmail.text.toString().trim()
+        password = binding.editTextPassword.text.toString().trim()
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.editTextEmail.error = "Email Invalid"
+            binding.editTextEmail.requestFocus()
+        }else if(email.isEmpty()){
+            binding.editTextEmail.error = "Enter Email"
+            binding.editTextEmail.requestFocus()
+        }else if(password.isEmpty()){
+            binding.editTextPassword.error = "Enter Password"
+            binding.editTextPassword.requestFocus()
+        }
+        else{
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+        progressDialog.setMessage("Login...")
+        progressDialog.show()
+
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                startActivity(Intent(this,MainActivity::class.java))
+                finishAffinity()
+                Toast.makeText(this,"Welcome", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {e->
+                progressDialog.dismiss()
+                Toast.makeText(this,"Failed to log in ${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
